f6794b31195f255be6778775ebc3f9e0c859ff6d
jakabelaj1 <jaka.belaj@gmail.com>
2026-07-26 15:04:45 +0200

    react setup

---
+++ src/app/App.jsx
@@ -0,0 +1,15 @@
+import 'sass-reset';
+import './App.scss'
+
+function App() {
+
+    return (
+        <>
+            <section id="center">
+
+            </section>
+        </>
+    )
+}
+
+export default App
